e4f0f70d3195d7ff811aef6095efdeb190afcf57
Alan James <alanjames1987@gmail.com>
2012-09-26 19:37:40 -0400

    Worked on the MList functions.

--- bin/script.js
+++ bin/script.js
@@ -104,7 +104,7 @@ var rounded_large = '2em';$(window).load(function() {
 	var MList = {
 		fadeIn : function() {
 			var i = 0;
-			$(".MListItem").each(function() {
+			$('.MListItem').each(function() {
 				$(this).delay(i).animate({
 					opacity : 1
 				}, speedNorm);
@@ -112,7 +112,7 @@ var rounded_large = '2em';$(window).load(function() {
 			});
 
 			var i = 0;
-			$("#MSidebar li").each(function() {
+			$('#MSidebar li').each(function() {
 				$(this).delay(i).animate({
 					opacity : 1
 				}, speedNorm);
@@ -120,44 +120,35 @@ var rounded_large = '2em';$(window).load(function() {
 			});
 		},
 		selectAll : function(selector) {
-			console.log(selector);
-			$(selector + " .MListItem input[type=checkbox]:visible").each(function() {
-				$(this).attr("checked", "checked");
-				MList.updateUI(selector);
+
+			// Set a deault parameter
+			selector = typeof selector !== 'undefined' ? selector : '';
+
+			$(selector + ' .MListItem input[type=checkbox]:visible').each(function() {
+				$(this).attr('checked', 'checked');
+				MList.updateUI();
 			});
 		},
 		deselectAll : function(selector) {
-			console.log(selector);
+
+			// Set a deault parameter
+			selector = typeof selector !== 'undefined' ? selector : '';
+
 			$(selector + ' .MListItem input[type=checkbox]').each(function() {
 				$(this).removeAttr('checked');
-				MList.updateUI(selector);
+				MList.updateUI();
 			});
 		},
-		updateUI : function(selector) {
+		updateUI : function() {
 
-			// Check if anything was passed in
-			if (selector === undefined) {
-				selector = '';
-			}
-
-			$(selector + '.MListItem input[type=checkbox]').each(function(index) {
+			$('.MListItem input[type=checkbox]').each(function(index) {
 
 				// Set the default state
-				if ($(this).is(":checked")) {
-					$(this).closest('.MListItem').addClass('blue');
-				}else{
-					$(this).closest('.MListItem').removeClass('blue');
+				if ($(this).is(':checked')) {
+					$(this).closest('.MListItem').addClass('active');
+				} else {
+					$(this).closest('.MListItem').removeClass('active');
 				}
-
-				// Add a change event listener
-				// A label can change the state of the checkbox so I am using change instead of click
-				$(this).live('change', function() {
-					if ($(this).is(":checked")) {
-						$(this).closest('.MListItem').addClass('blue');
-					} else {
-						$(this).closest('.MListItem').removeClass('blue');
-					}
-				});
 
 			});
 
@@ -171,19 +162,15 @@ var rounded_large = '2em';$(window).load(function() {
 // Setup checked when the page loads
 $(document).ready(function() {
 
-	$('.MListItem input[type=checkbox]').each(function(index) {
+	// Set the default state
+	MList.updateUI();
 
-		// Set the default state
-		MList.updateUI();
+	$('.MListItem input[type=checkbox]').each(function(index) {
 
 		// Add a change event listener
 		// A label can change the state of the checkbox so I am using change instead of click
-		$(this).live('change', function() {
-			if ($(this).is(":checked")) {
-				$(this).closest('.MListItem').addClass('blue');
-			} else {
-				$(this).closest('.MListItem').removeClass('blue');
-			}
+		$(this).change(function() {
+			MList.updateUI();
 		});
 
 	});
--- src/ui/MList.js
+++ src/ui/MList.js
@@ -3,7 +3,7 @@
 	var MList = {
 		fadeIn : function() {
 			var i = 0;
-			$(".MListItem").each(function() {
+			$('.MListItem').each(function() {
 				$(this).delay(i).animate({
 					opacity : 1
 				}, speedNorm);
@@ -11,7 +11,7 @@
 			});
 
 			var i = 0;
-			$("#MSidebar li").each(function() {
+			$('#MSidebar li').each(function() {
 				$(this).delay(i).animate({
 					opacity : 1
 				}, speedNorm);
@@ -19,44 +19,35 @@
 			});
 		},
 		selectAll : function(selector) {
-			console.log(selector);
-			$(selector + " .MListItem input[type=checkbox]:visible").each(function() {
-				$(this).attr("checked", "checked");
-				MList.updateUI(selector);
+
+			// Set a deault parameter
+			selector = typeof selector !== 'undefined' ? selector : '';
+
+			$(selector + ' .MListItem input[type=checkbox]:visible').each(function() {
+				$(this).attr('checked', 'checked');
+				MList.updateUI();
 			});
 		},
 		deselectAll : function(selector) {
-			console.log(selector);
+
+			// Set a deault parameter
+			selector = typeof selector !== 'undefined' ? selector : '';
+
 			$(selector + ' .MListItem input[type=checkbox]').each(function() {
 				$(this).removeAttr('checked');
-				MList.updateUI(selector);
+				MList.updateUI();
 			});
 		},
-		updateUI : function(selector) {
+		updateUI : function() {
 
-			// Check if anything was passed in
-			if (selector === undefined) {
-				selector = '';
-			}
-
-			$(selector + '.MListItem input[type=checkbox]').each(function(index) {
+			$('.MListItem input[type=checkbox]').each(function(index) {
 
 				// Set the default state
-				if ($(this).is(":checked")) {
-					$(this).closest('.MListItem').addClass('blue');
-				}else{
-					$(this).closest('.MListItem').removeClass('blue');
+				if ($(this).is(':checked')) {
+					$(this).closest('.MListItem').addClass('active');
+				} else {
+					$(this).closest('.MListItem').removeClass('active');
 				}
-
-				// Add a change event listener
-				// A label can change the state of the checkbox so I am using change instead of click
-				$(this).live('change', function() {
-					if ($(this).is(":checked")) {
-						$(this).closest('.MListItem').addClass('blue');
-					} else {
-						$(this).closest('.MListItem').removeClass('blue');
-					}
-				});
 
 			});
 
@@ -70,19 +61,15 @@
 // Setup checked when the page loads
 $(document).ready(function() {
 
-	$('.MListItem input[type=checkbox]').each(function(index) {
+	// Set the default state
+	MList.updateUI();
 
-		// Set the default state
-		MList.updateUI();
+	$('.MListItem input[type=checkbox]').each(function(index) {
 
 		// Add a change event listener
 		// A label can change the state of the checkbox so I am using change instead of click
-		$(this).live('change', function() {
-			if ($(this).is(":checked")) {
-				$(this).closest('.MListItem').addClass('blue');
-			} else {
-				$(this).closest('.MListItem').removeClass('blue');
-			}
+		$(this).change(function() {
+			MList.updateUI();
 		});
 
 	});
