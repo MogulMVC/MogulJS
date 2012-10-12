ff292f7813b6121dc5b573850f41d036b2bdfc1c
Alan James <alanjames1987@gmail.com>
2012-10-12 02:11:31 -0400

    Worked on the note manager.

--- src/manager/MNoteManager.js
+++ src/manager/MNoteManager.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.MNote').each(function() {
+	$('.MNote, .MNoteRed, .MNoteR, .MNoteOrange, .MNoteO, .MNoteYellow, .MNoteY, .MNoteGreen, .MNoteG, .MNoteBlue, .MNoteB, .MNoteViolet, .MNoteV').each(function() {
 		var rotation = MMath.random(-8, 8);
 		$(this).css({
 			'-webkit-transform' : 'rotate(' + rotation + 'deg)',
