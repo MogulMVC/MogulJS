c952f6f2f49c1a70f834520e79bc1b7b6f9914eb
Alan James <alanjames1987@gmail.com>
2012-10-20 12:24:01 -0400

    Auto Commit

--- src/util/MURL.js
+++ src/util/MURL.js
@@ -8,9 +8,17 @@
 		segment : function(segement) {
 			var pathArray = window.location.pathname.split('/');
 			var secondLevelLocation = pathArray[segement];
+		},
+		reset : function() {
+			var answer = confirm("All your changes will be disgarded.\nAre you sure you want to reset?");
+
+			if (answer) {
+				location.reload(true);
+				return true
+			};
 		}
 	}
-	
+
 	window.MURL = MURL;
 
 })();
