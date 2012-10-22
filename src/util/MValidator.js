83516a49bc85c227a0269b5713e3a7df742c1cb9
Alan James <alanjames1987@gmail.com>
2012-10-22 07:29:22 -0400

    Auto Commit

---
+++ src/util/MValidator.js
@@ -0,0 +1,13 @@
+(function() {
+
+	var MValidator = {
+
+		email : function(email) {
+			var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+			return reg.test(email);
+		}
+	};
+	
+	window.MValidator = MValidator;
+
+})();
