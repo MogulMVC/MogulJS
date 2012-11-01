effe5ecc0b43b6470f687a92480d9fc0fbf6ff11
WDDBS <wddbs@sep001c58fb5ccc.fullsail.com>
2012-10-31 21:47:08 -0400

    Fixed the MValidate class.

--- src/util/MValidate.js
+++ src/util/MValidate.js
@@ -1,6 +1,6 @@
 (function() {
 
-	var MValidator = {
+	var MValidate = {
 
 		email : function(email) {
 			var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
@@ -8,6 +8,6 @@
 		}
 	};
 	
-	window.MValidator = MValidator;
+	window.MValidate = MValidate;
 
 })();
