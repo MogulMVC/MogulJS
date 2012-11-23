5ca511478fcdb6c11961a3a4ed6f2ee81be313b5
Alan James <alanjames1987@gmail.com>
2012-11-23 12:19:11 -0500

    Fixed some popup stuff

--- src/manager/MPopupManager.js
+++ src/manager/MPopupManager.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+
+	$('.MPopupContainer').live('click', function() {
+		$(this).hide();
+	});
+
 	$('.MPopup .MIconClose').live('click', function() {
 		$(this).closest('.MPopupContainer').hide();
 	});
+
 });
