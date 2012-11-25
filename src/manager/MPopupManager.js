a796822ea44e50af16a8c62a5be2b4a5cfb28f45
Alan James <alanjames1987@gmail.com>
2012-11-24 21:40:17 -0500

    Commit

--- src/manager/MPopupManager.js
+++ src/manager/MPopupManager.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-	$('.MPopupContainer').live('click', function() {
-		$(this).hide();
-	});
-
 	$('.MPopup .MIconClose').live('click', function() {
 		$(this).closest('.MPopupContainer').hide();
 	});
