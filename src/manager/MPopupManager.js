201fe49c1ccb23deaec3baa891154a4bcbaf6e95
Alan James <alanjames1987@gmail.com>
2012-12-10 12:08:47 -0500

    Worked on some popup logic.

--- src/manager/MPopupManager.js
+++ src/manager/MPopupManager.js
@@ -3,5 +3,11 @@ $(document).ready(function() {
 	$('.MPopup .MIconClose').live('click', function() {
 		$(this).closest('.MPopupContainer').hide();
 	});
+	
+	$(document).keyup(function(e) {
+		if(e.keyCode == 27){
+			$('.MPopupContainer').hide();
+		};
+	});
 
 });
