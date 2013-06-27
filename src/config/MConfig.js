87838db1b8bbc9604b964cd8e9811acbd94ae6f6
Alan James <alanjames1987@gmail.com>
2013-06-27 07:43:37 -0400

    Commit

--- src/config/MConfig.js
+++ src/config/MConfig.js
@@ -1,6 +1,6 @@
 /*
  * MScript.js by Alan James
- * version 130626
+ * version 130627
  * recommended jQuery version 1.9.0
  */
 
@@ -8,7 +8,7 @@
 
 	var MConfig = {};
 
-	MConfig.version = '130626';
+	MConfig.version = '130627';
 
 	// Speed
 	MConfig.speedFast = 125;
